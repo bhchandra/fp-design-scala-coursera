9ff34a2d31fbe282d1252bce6c15da89ede1210c
Mitra <bhanumchandra@gmail.com>
2018-06-30 23:14:33 -0500

    implement TreeGenerator.

---
+++ src/main/scala/week1_For_Exp_and_Monads/TreeGenerator.scala
@@ -0,0 +1,36 @@
+package week1_For_Exp_and_Monads
+
+import java.util.concurrent.ThreadLocalRandom
+
+import week1_For_Exp_and_Monads.Week1Lab.Generator
+
+object TreeGenerator {
+
+  trait Tree
+
+  case class Inner(left: Tree, right: Tree) extends Tree
+
+  case class Leaf(x: Int) extends Tree
+
+  val integers: Generator[Int] = new Generator[Int] {
+    val rand = new ThreadLocalRandom
+
+    override def generate: Int = rand.nextInt()
+  }
+
+  def trees: Generator[Tree] = {
+    for {
+      isLeaf <- integers.map(x => x > 0)
+      tree <- if (isLeaf) leafs else inners
+    } yield tree
+
+    def leafs: Generator[Tree] = for (x <- integers) yield Leaf(x)
+
+    def inners: Generator[Tree] =
+      for {
+        left <- leafs
+        right <- leafs
+      } yield Inner(left, right)
+  }
+
+}
